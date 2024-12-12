4bd16e0f0a9ae36709a179d8a49dcc06726ac46a
Andrewchidi <andrewchidiebere73@gmail.com>
2024-12-12 03:13:42 -0500

    social media

---
+++ config/db.js
@@ -0,0 +1,8 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+export const connectDb = async () => {
+  await mongoose.connect(process.env.MONGO_URL)
+    .then(() => console.log("mongodb connected"))
+    .catch((error) => console.log(error));
+};
---
+++ index.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import bodyParser from "body-parser";
+import { connectDb } from "./config/db.js";
+dotenv.config();
+
+
+const app = express();
+
+connectDb();
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.json());
+const port = process.env.PORT||5000;
+
+
+
+app.listen(port, ()=> {
+  console.log(`server is active on port ${port}`);
+})
+
